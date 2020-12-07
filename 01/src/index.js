8264b2e35d23c487b93fd9acb7680244ef67d7ef
liwen <liwen111111>
2020-12-07 22:14:46 +0800

    01上传

---
+++ 01/src/index.js
@@ -0,0 +1,4 @@
+console.log('hello webpack')
+
+// webpack的默认配置
+// 1. webpack执行构建会找webpack.config.js 这个配置文件， 如果没有找到则走默认配置
---
+++ 01/webpack.config.js
@@ -0,0 +1,14 @@
+// npx 指向node_modules下的webpack文件夹的bin 下面的webpack.js
+
+// webpack配置就是一个对象
+// webpack是基于nodeJs
+const path = require('path')
+
+module.exports = {
+  context: process.cwd(), // 上下文 项目打包相对路径， 默认指向项目的根目录即：process.cwd()，必须是绝对路径
+  entry: './src/index.js', // 打包构建的入口，是相对路径，相对于context路径
+  output: { // 出口
+    path: path.resolve(__dirname, './build'), // 构建的文件放在哪里，必须是绝对路径
+    filename: 'index.js' // 构建的文件名称
+  }
+}
